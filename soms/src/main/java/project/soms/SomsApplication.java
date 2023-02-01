35ef1979c264fdce77bca369169bbde387c08d98
TaeyangYeon <acrobatyeon@gmail.com>
2023-02-01 14:52:29 +0900

    프로젝트 재빌드

--- soms/src/main/java/project/soms/SomsApplication.java
+++ soms/src/main/java/project/soms/SomsApplication.java
@@ -1,4 +1,4 @@
-package com.soms.soms;
+package project.soms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- soms/src/test/java/project/soms/SomsApplicationTests.java
+++ soms/src/test/java/project/soms/SomsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.soms.soms;
+package project.soms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
