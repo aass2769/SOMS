4d3183580a1f3484ddc6eab0925884bc6992ccf3
TaeyangYeon <acrobatyeon@gmail.com>
2023-02-22 17:36:00 +0900

    load test 삭제....

--- soms/src/test/java/project/soms/SomsApplicationTests.java
+++ soms/src/test/java/project/soms/SomsApplicationTests.java
@@ -1,13 +1,9 @@
 package project.soms;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SomsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
